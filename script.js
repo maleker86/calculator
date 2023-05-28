a4ecc0e766c5b66e8b79c5f58c77d575ecf7e3bf
Kris <109919530+maleker86@users.noreply.github.com>
2023-05-28 15:07:01 -0700

    Initiate Operands/Operators/Operate HTML

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello")
+let operand1 = "";
+let operand2 = "";
+let operator = "";
+const operands = [];
+const operators = [];
+
+//array.unshift() is opposite of array.push()
+
+//event listeners & button functions
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    if (button.className === "operand") {
+      if (!operator) {
+        if (!operand1) {
+          operand1 = button.textContent;
+          console.log(`o1 is ${operand1}`);
+        } else {
+          operand1 += button.textContent;
+          console.log(`o1 is ${operand1}`);
+        }
+      } else {
+        if (!operand2) {
+          operand2 = button.textContent;
+          console.log(`o2 is ${operand2}`);
+        } else {
+          operand2 += button.textContent;
+          console.log(`o2 is ${operand2}`);
+        }
+      }
+    }
+    if (button.className === "operator") {
+      if (!operator) {
+        operator = button.textContent;
+        console.log(`o3 is ${operator}`);
+      } else {
+        return;
+      }
+    }
+  });
+});
