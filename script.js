00cde68e2d83c607c74320482d952edd1f6c9c3d
Kris <109919530+maleker86@users.noreply.github.com>
2023-06-01 18:42:16 -0700

    Print operators to screen; continue destroyed FlexBox

--- script.js
+++ script.js
@@ -2,46 +2,32 @@ let operator = "";
 let currentNumber = [];
 let equation = [];
 let solution;
+let resultDisplay = document.getElementById("results");
 
-//press numbers
-//add numbers to currentNumber
-//press + etc
-//add numbers to equation, joined
-//remove numbers from currentNumber
-//press numbers
-//add numbers to currentNumber
-//press =
-//add numbers to equation, joined
-//remove numbers from currentNumber
-//equate them
-//show a solution
-//add solution to equation
-//clear to clear the number
-
-//array.unshift() is opposite of array.push()
-
-//event listeners & button functions
 document.querySelectorAll("button").forEach((button) => {
   button.addEventListener("click", (event) => {
     if (button.className === "operand") {
       let number = Number(button.textContent);
       currentNumber.push(number);
-    //  console.log(typeof currentNumber[0]);
-      console.log("current number is: ");
-      console.table(currentNumber);
+      // console.log(typeof currentNumber[0]);
+      // console.log("current number is: ");
+      // console.table(currentNumber);
+      // console.log(currentNumber[currentNumber.length - 1])
+      resultDisplay.textContent = currentNumber[currentNumber.length - 1];
     }
+
     if (button.className === "operator") {
       operator = button.textContent;
-      console.log(`operator is ${operator}`);
-      
+      //console.log(`operator is ${operator}`);
       if (currentNumber.length !== 0) {
         getCurrentNumber();
       }
-      }
+    }
     if (button.className === "operate") {
       getCurrentNumber();
       if (operator) {
         operate();
+        resultDisplay.textContent = solution;
         clearEquation();
         console.log("equation is:");
         console.table(equation);
@@ -51,7 +37,6 @@ document.querySelectorAll("button").forEach((button) => {
         solution = "";
         console.log("solution is:");
         console.log(solution);
-
         operator = "";
         console.log("operator is:");
         console.log(operator);
@@ -60,18 +45,26 @@ document.querySelectorAll("button").forEach((button) => {
       }
     }
     if (button.className === "clear") {
-     clearAll();
+      clearAll();
     }
   });
 });
 
 function getCurrentNumber() {
   equation.push(Number(currentNumber.join("")));
-  console.log("equation is:");
-  console.table(equation);
+
+  //console.log("equation is:");
+
+  resultDisplay.textContent = equation;
+
+  //console.table(equation);
+
   // console.log(typeof equation[0]);
+
   currentNumber = [];
+
   console.log("currentnumber is:");
+
   console.table(currentNumber);
 }
 
@@ -87,11 +80,10 @@ function operate() {
       break;
     case operator === "/":
       if (equation[1] === 0) {
-          console.log("No");
-          clearAll();
-          return;
-        } 
-
+        console.log("No");
+        clearAll();
+        return;
+      }
       solution = equation[0] / equation[1];
       solution = Math.round(solution * 100) / 100;
       break;
@@ -103,21 +95,32 @@ function operate() {
       console.log("no operator");
       break;
   }
+
   console.log(`solution is: ${solution}`);
-  return solution; 
+
+  return solution;
+
   //console.log(typeof solution);
 }
 
 function clearEquation() {
   equation = [];
+
   console.log("equation is:");
+
   console.table(equation);
 }
 
 function clearAll() {
   operator = "";
+
   number = "";
+
   currentNumber = [];
+
   equation = [];
+
   solution = "";
+
+  resultDisplay.textContent = "";
 }
